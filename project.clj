8be2f6cbc7f42721aa04dc002ba7af595da1fc8e
Morten Kristoffersen <morten.kristoffersen@ice.net>
2021-05-04 19:10:37 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject net.clojars.mokr/plug-debug "0.1.0-SNAPSHOT"
+  :description "A Clojure and Clojurescript library with small debug helpers"
+  :url "https://github.com/mokr/plug-debug"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.844" :scope "provided"]]
+  :repl-options {:init-ns plug-debug.core})
---
+++ test/plug_debug/core_test.clj
@@ -0,0 +1,7 @@
+(ns plug-debug.core-test
+  (:require [clojure.test :refer :all]
+            [plug-debug.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
